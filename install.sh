841fe0be5eec58e547847948726cdbc7945b5946
yayuuu <yayuuu@gmail.com>
2025-06-30 22:45:16 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
 install(){
-  cp hstr "${binpath}hstr"
-  chmod +x "${binpath}hstr"
+  cp hb "${binpath}hb"
+  chmod +x "${binpath}hb"
   echo ""
   echo "Installation complete. Restart your bash session to activate alias (if selected)."
-  echo "Use \"hstr [search_phrase]\" to search in history."
-  echo "If you installed hstr locally, make sure \"~/.local/bin\" is in your PATH."
+  echo "Use \"hb [search_phrase]\" or \"hstr [search_phrase]\" to search in history."
+  echo "If you installed History Browser locally, make sure \"~/.local/bin\" is in your PATH."
   echo "If not, add the following line at the end of your .bashrc:"
   echo "export PATH=\"\$HOME/.local/bin:\$PATH\""
   exit
 }
 
 install_with_alias() {
-  echo "alias hstr=\"history -a && hstr\"" >> "$aliasfile"
+  echo "alias hb=\"history -a && hb\"" >> "$aliasfile"
+  echo "alias hstr=\"history -a && hb\"" >> "$aliasfile"
   install
 }
 
 
-echo "Do you want to install hstr globally?"
+echo "Do you want to install History Browser globally?"
 select opt in "Yes" "No"; do
   case $opt in
     "Yes")
@@ -45,7 +46,7 @@ done
 
 echo ""
 echo "Do you want to install alias?"
-echo "An alis is required to save current session's history before executing hstr."
+echo "An alis is required to save current session's history before executing hb."
 select opt in "Yes" "No"; do
   case $opt in
     "Yes")  
